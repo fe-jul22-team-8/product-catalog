f7845071774b9d20ce568c5ecaa0dd481b11ffb3
Vasyl Zinchenko <53319174+VasylZinchenko@users.noreply.github.com>
2022-11-03 14:41:24 +0200

    Update phones.js

--- server/src/service/phones.js
+++ server/src/service/phones.js
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import path from 'path';
 
-const filePath = path.resolve('api', 'phones.json');
+const filePath = path.resolve('src', 'api', 'phones.json');
 
 function read() {
   const data = fs.readFileSync(filePath, 'utf-8');
