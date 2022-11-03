0ecde0a58f7bc142a4e3e4b271e437a6b3573b21
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-11-03 09:54:27 +0200

    changing server

--- server/src/router/phones.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-// import express from 'express';
-import * as phoneController from '../controller/phones.js';
-// export const showPhones = (app: Express) => {
-export const showPhones = (app) => {
-    const router = express.Router();
-    app.use('/phones', router);
-    router.get('/', phoneController.getMany);
-};
