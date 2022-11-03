e27270461730448974563819bea500589cd9fcab
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-11-03 11:28:53 +0200

    updating

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import express from 'express';
 dotenv.config();
 
 const app = server.createServer();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5050;
 
 app.use(express.static('./src'));
 
