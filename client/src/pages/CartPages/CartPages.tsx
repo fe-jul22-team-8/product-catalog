1f2e386939df683074cd56415f2d6f200516de0f
Andrew Zaitsev <alzay007@gmail.com>
2022-11-03 12:49:05 +0200

    my changes

--- client/src/pages/CartPages/CartPages.tsx
+++ client/src/pages/CartPages/CartPages.tsx
@@ -1,25 +1,45 @@
-import style from './CartPages.module.scss';
-import arrow from '../../icons/arrowRight.svg';
+import styles from './CartPages.module.scss';
+import arrow from '../../icons/greyArrowLeft.svg';
+import { useContext } from 'react';
+import { CardContext } from '../../context/CardContext';
 
 export const CartPages = () => {
+  const { setCardData, cardData } = useContext(CardContext);
+
   return (
-    <div className={style.cartPages}>
-      <div className={style.title}>
-        <div className={style.title__navigation}>
-          <img src={arrow} alt="arrow" className={style.title__arrow} />
-          <p className={style.title__arrowText}>Back</p>
+    <>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <div className={styles.title__navigation}>
+            <img
+              src={arrow}
+              alt="arrow"
+              className={styles.title__arrow}
+            />
+            <p className={styles.title__arrowText}>Back</p>
+          </div>
+          <h1 className={styles.title__headline}>Cart</h1>
         </div>
-        <h1 className={style.title__headline}>Cart</h1>
+        <div className={styles.CartContainer}>
+          <div className={styles.CartPages}>
+            <section className={styles.CartPages__wrapper}>
+              <div className={styles.CartPages__cardWrapper}>
+                <div className={styles.CartPages__card}>cart</div>
+                <div className={styles.CartPages__card}>cart</div>
+                <div className={styles.CartPages__card}>cart</div>
+                <div className={styles.CartPages__card}>cart</div>
+              </div>
+              <div className={styles.checkout}>
+                <h3 className={styles.checkout__total}>
+                  ${cardData.reduce((a, b) => a + b.price, 0)}
+                </h3>
+                <p className={styles.checkout__count}>Total for {cardData.length} items</p>
+                <div className={styles.checkout__button}>Checkout</div>
+              </div>
+            </section>
+          </div>
+        </div>
       </div>
-      <section className={style.cartPages__wrapper}>
-        <div className={style.cartPages__wrapperCard}>
-          <div className={style.cartPages__card}>cart</div>
-          <div className={style.cartPages__card}>cart</div>
-          <div className={style.cartPages__card}>cart</div>
-          <div className={style.cartPages__card}>cart</div>
-        </div>
-        <div className={style.cartPages__wrapperCost}>cost</div>
-      </section>
-    </div>
+    </>
   );
 };
