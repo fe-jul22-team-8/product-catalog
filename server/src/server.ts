7e617d7c6a8e98a5e20d5ababe6a1f1cac0592d0
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-11-04 03:20:19 +0200

    changing localhost to heroku and adding :id endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,10 @@
 import dotenv from 'dotenv';
-import * as server from './createServer.js';
-import express from 'express';
+import {createServer} from './createServer.js';
 
 dotenv.config();
 
-const app = server.createServer();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./src'));
-
-app.get('/', (req, res) => {
-  res.send('Hi');
-});
-
-app.listen(port, () => {
+createServer().listen(port, () => {
   console.log(`⚡️ Server is running at http://localhost:${port}`);
 });
