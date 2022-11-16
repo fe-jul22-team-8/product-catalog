4cd27d47942b91c91ef8cd228250f4ce196a6505
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-11-16 12:11:25 +0200

    adding components on productPage

---
+++ client/src/components/ColorSize/ColorSize.tsx
@@ -0,0 +1,133 @@
+import styles from './ColorSize.module.scss';
+import classNames from 'classnames';
+import { useState } from 'react';
+import { Card } from '../Card';
+
+import { BASE_URL } from '../../utils/fetchProducts';
+import { useContext } from 'react';
+import { CardContext } from '../../context/CardContext';
+import { Phone } from '../../types/Phone';
+import { Link } from "react-router-dom";
+
+
+export const capacities = [
+  { id: '1', title: '64GB' },
+  { id: '2', title: '256GB' },
+  { id: '3', title: '512GB' },
+];
+
+export const colors = [
+  { id: '1', title: '#FCDBC1' },
+  { id: '2', title: '#5F7170' },
+  { id: '3', title: '#4C4C4C' },
+  { id: '4', title: '#F0F0F0' },
+];
+
+export const ColorSize: React.FC = () => {
+  const { setCardData, cardData } = useContext(CardContext);
+  const [isActiveColor, setIsActiveColor] = useState('1');
+  const [isActiveCapacity, setIsActiveCapacity] = useState('1');
+
+   return (
+    <>
+        <div className={styles.ActionBlock}>
+      <div className={styles.ColorBlock}>
+        <span className={styles.ActionBlock__option_text}>
+          Available Colors
+        </span>
+
+        <ul className={styles.ColorBlock__list}>
+          {colors.map((color) => (
+            <li
+              key={color.id}
+              onClick={() => setIsActiveColor(color.id)}
+              className={
+                isActiveColor === color.id
+                  ? styles.ColorBlock__list__item_active
+                  : styles.ColorBlock__list__item
+              }
+            >
+              <a
+                className={styles.ColorBlock__list__item__link}
+                style={{ backgroundColor: color.title }}
+              ></a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.ActionBlock__underline}></div>
+
+      <div className={styles.CapacityBlock}>
+        <span className={styles.ActionBlock__option_text}>Select capacity</span>
+
+        <ul className={styles.CapacityBlock__list}>
+          {capacities.map((capacity) => (
+            <li 
+              key={capacity.id}
+              onClick={() => setIsActiveCapacity(capacity.id)}
+              className={
+                isActiveCapacity === capacity.id
+                  ? styles.CapacityBlock__list__item_active
+                  : styles.CapacityBlock__list__item
+              }
+            >
+              <a 
+              className={
+                isActiveCapacity === capacity.id
+                  ? styles.CapacityBlock__list__item__link_active
+                  : styles.CapacityBlock__list__item__link
+              }
+              
+              >
+                {capacity.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.action_block__underline}></div>
+      </div>
+
+      <div className={styles.ActionBlock__underline}></div>
+
+    </div>
+
+
+    <div className={styles.card}>
+      <div className={styles.card_price}>
+        <span className={styles.card_newPrice}>$799</span>
+        <span className={styles.card_oldPrice}>$1199</span>
+      </div>
+
+      <div className={styles.card_buttons}>
+      <button className={styles.card_checkout}>Add to card</button>
+        <button className={styles.card_wishlist}></button>
+      </div>
+
+      <div className={styles.card_description}>
+        <span className={styles.card_text}>Screen</span>
+        <span className={styles.card_value}>6.5” OLED</span>
+      </div>
+
+      <div className={styles.card_description}>
+        <span className={styles.card_text}>Resolution</span>
+        <span className={styles.card_value}>2688x1242</span>
+      </div>
+
+      <div className={styles.card_description}>
+        <span className={styles.card_text}>Processor</span>
+        <span className={styles.card_value}>Apple A12 Bionic</span>
+      </div>
+      
+      <div className={styles.card_description}>
+        <span className={styles.card_text}>RAM</span>
+        <span className={styles.card_value}>3 GB</span>
+      </div>
+    </div>
+    
+    </>
+
+  );
+};
+
--- client/src/pages/ProductDetalePage/ProductDetalePage.tsx
+++ client/src/pages/ProductDetalePage/ProductDetalePage.tsx
@@ -1,13 +1,25 @@
 import { useParams } from 'react-router-dom';
+import { PhotosBlockSelecting } from '../../components/PhotosBlockSelecting'
 import style from './ProductDetalePage.module.scss';
+import { useContext } from 'react';
+import { PhonesDataContext } from '../../context/DataContext';
+import { Phone } from '../../types/Phone';
+import { ColorSize } from '../../components/ColorSize';
 
 export const ProductDetalePage = () => {
+  const { phonesList, isLoading } = useContext(PhonesDataContext);
   const { productId } = useParams();
+  const currentPhone = phonesList.find(phone => phone.id === productId);
+
   return (
     <div className={style.wrapper}>
-      <div className={style.title}>title</div>
-      <div className={style.photo}>photo</div>
-      <div className={style.variant}>Variants/acions block</div>
+      <div className={style.title}>{currentPhone?.name}</div>
+      <div className={style.photo}>
+        <PhotosBlockSelecting />
+      </div>
+      <div className={style.variant}>
+        <ColorSize />
+      </div>
       <div className={style.about}>about</div>
       <div className={style.tech}>Tech specs</div>
       <div className={style.like}>Recommended goods</div>
