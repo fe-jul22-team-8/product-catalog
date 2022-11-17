bf6280420bf89965f33ff4a463f2e4254818b2af
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-11-17 02:30:45 +0200

    fixing loading main picture

--- client/src/components/PhotosBlockSelecting/PhotosBlockSelecting.tsx
+++ client/src/components/PhotosBlockSelecting/PhotosBlockSelecting.tsx
@@ -1,17 +1,16 @@
 import styles from './PhotosBlockSelecting.module.scss';
 import { PhonesDataContext } from '../../context/DataContext';
 import { BASE_URL } from '../../utils/fetchProducts';
-import { Phone } from '../../types/Phone';
 import { useState, useContext } from 'react';
-import { useOutletContext, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export const PhotosBlockSelecting = () => {
   const { phonesList, isLoading } = useContext(PhonesDataContext);
   const { productId } = useParams();
   const currentPhone = phonesList.find((phone) => phone.id === productId);
-  const [currentImg, setCurrentImg] = useState(`${BASE_URL}/${currentPhone?.image}`);
   const images = currentPhone?.images;
-
+  const [currentImg, setCurrentImg] = useState('');
+  console.log(currentImg);
 
   const handleClick = (event: any) => {
     setCurrentImg(event.currentTarget.getAttribute('src'));
@@ -21,6 +20,7 @@ export const PhotosBlockSelecting = () => {
       <div className={styles.PhotosBlock__sidePanel}>
         {images?.map(img => (
           <img
+          key={img}
           src={`${BASE_URL}/${img}`}
           className={styles.PhotosBlock__sidePanel_photo}
           onClick={handleClick}
@@ -29,7 +29,9 @@ export const PhotosBlockSelecting = () => {
 
       </div>
       <div className={styles.PhotosBlock__mainPhoto}>
-        <img src={currentImg} className={styles.PhotosBlock__mainPhoto_photo} />
+        <img 
+          src={currentImg || (images && `${BASE_URL}/${images[0]}`)} 
+          className={styles.PhotosBlock__mainPhoto_photo} />
       </div>
     </div>
   );
