0bac1d35d28daadd7d909d8d523ef2b568196a0b
Shahmatov David <davidsahmatov4@gmail.com>
2022-11-24 16:55:49 +0300

    Recommended goods without swiper

---
+++ client/src/components/RecommendedGoods/RecommendedGoods.tsx
@@ -0,0 +1,213 @@
+import recommendedGoods from '../../images/recommendedGood.png';
+import './RecommendedGoods.scss';
+
+export function RecommendedGoods() {
+  return (
+    <>
+      <div className="recommendedGood">
+        <div className="recommendedGoods__top">
+          <p className="recommendedGoods__top_text">You may also like</p>
+          <div className="recommendedGoods__top_buttons">
+            <button className="recommendedGoods__top_button">&lt;</button>
+            <button className="recommendedGoods__top_button">&gt;</button>
+          </div>
+        </div>
+
+        <div className="recommendedGoods">
+          <div className='recommendedGoods__item'>
+            <img
+              src={recommendedGoods}
+              alt="card-logo"
+              className='recommendedGoods__item_img'
+            />
+            <span className='recommendedGoods__item_title'>
+              <p className='recommendedGoods__item_title_text'>
+                Apple iPhone 11 Pro Max 512GB Midnight Green (iMT9G2FS/A)
+              </p>
+            </span>
+            <div className='recommendedGoods__item_prices'>
+              <span className='recommendedGoods__item_prices_new'>$800</span>
+              <span className='recommendedGoods__item_prices_old'>$890</span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Screen
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                5.8” OLED
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Capacity
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                64 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>RAM</span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                4 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_bottom'>
+              <button
+                className='recommendedGoods__item_bottom_checkout'
+              >
+                Add to cart
+              </button>
+              <button
+                className='recommendedGoods__item_bottom_wishlist'
+              ></button>
+            </div>
+          </div>
+          <div className='recommendedGoods__item'>
+            <img
+              src={recommendedGoods}
+              alt="card-logo"
+              className='recommendedGoods__item_img'
+            />
+            <span className='recommendedGoods__item_title'>
+              <p className='recommendedGoods__item_title_text'>
+                Apple iPhone 11 Pro Max 512GB Midnight Green (iMT9G2FS/A)
+              </p>
+            </span>
+            <div className='recommendedGoods__item_prices'>
+              <span className='recommendedGoods__item_prices_new'>$800</span>
+              <span className='recommendedGoods__item_prices_old'>$890</span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Screen
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                5.8” OLED
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Capacity
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                64 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>RAM</span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                4 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_bottom'>
+              <button
+                className='recommendedGoods__item_bottom_checkout'
+              >
+                Add to cart
+              </button>
+              <button
+                className='recommendedGoods__item_bottom_wishlist'
+              ></button>
+            </div>
+          </div>
+          <div className='recommendedGoods__item'>
+            <img
+              src={recommendedGoods}
+              alt="card-logo"
+              className='recommendedGoods__item_img'
+            />
+            <span className='recommendedGoods__item_title'>
+              <p className='recommendedGoods__item_title_text'>
+                Apple iPhone 11 Pro Max 512GB Midnight Green (iMT9G2FS/A)
+              </p>
+            </span>
+            <div className='recommendedGoods__item_prices'>
+              <span className='recommendedGoods__item_prices_new'>$800</span>
+              <span className='recommendedGoods__item_prices_old'>$890</span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Screen
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                5.8” OLED
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Capacity
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                64 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>RAM</span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                4 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_bottom'>
+              <button
+                className='recommendedGoods__item_bottom_checkout'
+              >
+                Add to cart
+              </button>
+              <button
+                className='recommendedGoods__item_bottom_wishlist'
+              ></button>
+            </div>
+          </div>
+          <div className='recommendedGoods__item'>
+            <img
+              src={recommendedGoods}
+              alt="card-logo"
+              className='recommendedGoods__item_img'
+            />
+            <span className='recommendedGoods__item_title'>
+              <p className='recommendedGoods__item_title_text'>
+                Apple iPhone 11 Pro Max 512GB Midnight Green (iMT9G2FS/A)
+              </p>
+            </span>
+            <div className='recommendedGoods__item_prices'>
+              <span className='recommendedGoods__item_prices_new'>$800</span>
+              <span className='recommendedGoods__item_prices_old'>$890</span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Screen
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                5.8” OLED
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>
+                Capacity
+              </span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                64 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_characteristics'>
+              <span className='recommendedGoods__item_characteristics_text'>RAM</span>
+              <span className='recommendedGoods__item_characteristics_text_value'>
+                4 GB
+              </span>
+            </div>
+            <div className='recommendedGoods__item_bottom'>
+              <button
+                className='recommendedGoods__item_bottom_checkout'
+              >
+                Add to cart
+              </button>
+              <button
+                className='recommendedGoods__item_bottom_wishlist'
+              ></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
